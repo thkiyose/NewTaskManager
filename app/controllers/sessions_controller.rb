ac8fcacd943bdbf2a2b4e9c4065e2087cfb91273
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 14:55:46 +0900

    sessions/destroyメソッドを定義

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   before_action :cannot_do_when_logged_in, only:[:new,:create]
+  before_action :cannot_do_when_logged_out, only:[:destroy]
+
   def new
   end
 
@@ -13,11 +15,22 @@ class SessionsController < ApplicationController
    end
   end
 
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path, notice: "ログアウトしました。また来てね"
+  end
+
   private
 
     def cannot_do_when_logged_in
       if session[:user_id]
         redirect_to user_path(session[:user_id])
+      end
+    end
+
+    def cannot_do_when_logged_out
+      if !session[:user_id]
+        redirect_to new_session_path
       end
     end
 end
