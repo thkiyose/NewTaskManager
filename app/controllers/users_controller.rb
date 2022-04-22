3aaeadfbfe1a98b23266f00c64aeb49c9239f8b7
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 13:53:12 +0900

    ユーザー登録時のフラッシュメッセージを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to new_user_path, notice: "ユーザー登録しました。ようこそ、#{@user.name}さん！"
     else
       render :new
     end
