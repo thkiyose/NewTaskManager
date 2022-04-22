1ddd27752c80996df605e7cad16286d0368453ee
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 11:17:10 +0900

    Userのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, length: {maximum: 10}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ }
 end
