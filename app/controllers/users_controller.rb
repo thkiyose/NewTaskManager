94f8fe6dabf9604303f605f64414fe41188a3076
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 15:06:35 +0900

    ユーザー登録時に自動的にログインするように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id), notice: "ユーザー登録しました。ようこそ、#{@user.name}さん！"
     else
       render :new
