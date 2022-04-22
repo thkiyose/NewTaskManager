558a27ea7094e278e754ccd332321081d498faf0
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 15:02:45 +0900

    ルートの遷移先をログインページに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new,:create,:show]
   resources :sessions, only:[:new,:create,:destroy]
-  root to: "users#new"
+  root to: "sessions#new"
 end
