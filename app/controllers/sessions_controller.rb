3d6d166be76e6ee3f106073896181df3345fffb7
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 14:24:00 +0900

    sessions/createアクションを定義し、ログイン出来るように

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect_to user_path(user.id), notice: "ログインに成功しました。おかえりなさい、#{user.name}!"
+   else
+     render :new
+   end
+  end
 end
