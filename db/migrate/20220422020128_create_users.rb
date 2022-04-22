3ecd37ea95a08ab63de9a228ab7e69642bbdc817
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 11:06:28 +0900

    usersテーブルのカラムを作成

--- db/migrate/20220422020128_create_users.rb
+++ db/migrate/20220422020128_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :name, not_null: true
+      t.string :email, not_null: true
       t.string :password_digest
 
       t.timestamps
+      t.index :email, name: "index_users_on_email", unique: true
     end
   end
 end
