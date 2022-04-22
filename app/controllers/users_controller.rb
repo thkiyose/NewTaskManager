10338f1774615bb3ed1a39144e461ebdf13004f1
haruka tezuka <th_kiyose@outlook.com>
2022-04-22 13:58:06 +0900

    usersのshowアクションとビューを作成、ユーザー登録後の遷移先をshowに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: :show
+
   def new
     @user = User.new
   end
@@ -6,15 +8,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path, notice: "ユーザー登録しました。ようこそ、#{@user.name}さん！"
+      redirect_to user_path(@user.id), notice: "ユーザー登録しました。ようこそ、#{@user.name}さん！"
     else
       render :new
     end
+  end
+
+  def show
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
