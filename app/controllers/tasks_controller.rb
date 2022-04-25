5568233e462a7fad5b348ab92a3a86b1bdebc722
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:42:51 +0900

    tasksのnewとcreateアクションを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,31 @@
 class TasksController < ApplicationController
-  before_action :set_user, only: [:index]
-  before_action :cannot_do_when_logged_out, only:[:index]
+  before_action :set_user, only: [:index,:new,:create]
+  before_action :cannot_do_when_logged_out, only:[:index,:new,:create]
 
   def index
     @tasks = @user.tasks
+  end
+
+  def new
+    @task = @user.tasks.build
+  end
+
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを追加しました。"
+    else
+      render :new
+    end
   end
 
   private
 
   def set_user
     @user = User.find_by(id:session[:user_id])
+  end
+
+  def task_params
+    params.require(:tasks).permit(:title,:importance,:limit,:detail)
   end
 end
