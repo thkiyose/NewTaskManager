afc1058b74366fc5bd4a3bd50d8cff3b5862a158
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:28:18 +0900

    tasksコントローラーを作成、tasks周りのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   resources :users, only: [:new,:create,:show]
   resources :sessions, only:[:new,:create,:destroy]
+  resources :tasks
   root to: "sessions#new"
 end
