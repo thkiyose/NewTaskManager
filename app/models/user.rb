46308d102917a3f75842c24b86f1c897fd1c666c
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:05:11 +0900

    UserとTask間のアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: {maximum: 10}
   validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ }
 end
