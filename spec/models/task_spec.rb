9dc04df79126af92208ba6c8e0d2cd5afc5b778a
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 15:27:00 +0900

    TaskのModelSpecを追記

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,17 +4,23 @@ RSpec.describe Task, type: :model do
   user = User.create(email:"test@email.com",password:"password")
   describe "title validations" do
     it "is invalid without a title" do
-      task = user.tasks.build(importance:0,limit:2022-05-20)
+      task = user.tasks.build(importance:0,limit:"2025-05-01")
       expect(task).to be_invalid
     end
     it "is invalid with a title longer than 100 letters" do
-      task = user.tasks.build(title:"a"*101,importance:0,limit:2022-05-20)
+      task = user.tasks.build(title:"a"*101,importance:0,limit:"2025-05-01")
       expect(task).to be_invalid
     end
   end
-  describe "impoetance validations" do
+  describe "importance validations" do
     it "is invalid without a importance level" do
-      task = user.tasks.build(title:"test",limit:2022-05-20)
+      task = user.tasks.build(title:"test",limit:"2025-05-01")
+      expect(task).to be_invalid
+    end
+  end
+  describe "limit validations" do
+    it "is invalid when a limit date is in the past" do
+      task = user.tasks.build(title:"test",limit:"2020-05-01")
       expect(task).to be_invalid
     end
   end
