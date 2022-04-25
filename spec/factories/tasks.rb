dc318a6f124e67f254f3db72b4f2749eca75603c
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:25:55 +0900

    tasksのバリデーションをテストするModelSpecの実施

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     title { "MyString" }
     detail { "MyText" }
-    importance { "" }
+    importance { 0 }
     limit { "2022-04-25" }
     user { "" }
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.create(email:"test@email.com",password:"password")
+  describe "title validations" do
+    it "is invalid without a title" do
+      task = user.tasks.build(importance:0,limit:2022-05-20)
+      expect(task).to be_invalid
+    end
+    it "is invalid with a title longer than 100 letters" do
+      task = user.tasks.build(title:"a"*101,importance:0,limit:2022-05-20)
+      expect(task).to be_invalid
+    end
+  end
+  describe "impoetance validations" do
+    it "is invalid without a importance level" do
+      task = user.tasks.build(title:"test",limit:2022-05-20)
+      expect(task).to be_invalid
+    end
+  end
 end
