067a2208c581c950ff4be2e45d3757f75ea35563
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:33:37 +0900

    tasks/indexアクションを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,14 @@
 class TasksController < ApplicationController
+  before_action :set_user, only: [:index]
+  before_action :cannot_do_when_logged_out, only:[:index]
+
   def index
+    @tasks = @user.tasks
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id:session[:user_id])
   end
 end
