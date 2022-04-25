7852c3479a0bdcba0605205440eae27b8bcfd806
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:12:56 +0900

    tasksのバリデーションを設定、enumを宣言

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  enum importance: { 低: 0, 中: 1, 高: 2 }
+  validates :title, presence: true, length: {maximum: 100}
+  validates :importance, presence: true
 end
