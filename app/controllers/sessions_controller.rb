cf6c52aa0e94aa5e3182afaf6c258a71ad981e3d
haruka tezuka <th_kiyose@outlook.com>
2022-04-25 14:36:40 +0900

    ログインとユーザー登録後の遷移先をタスク一覧に変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id), notice: "ログインに成功しました。おかえりなさい、#{user.name}!"
+      redirect_to tasks_path, notice: "ログインに成功しました。おかえりなさい、#{user.name}!"
    else
       flash.now[:notice] = "ログインに失敗しました。"
       render :new
